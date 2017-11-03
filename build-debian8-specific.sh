ee23f6deb095ad620af9327e4c32ca67611c4df1
Gianguido <g.sora4@gmail.com>
2017-11-03 15:57:19 +0100

    forgot a backslash

--- build-debian8-specific.sh
+++ build-debian8-specific.sh
@@ -2,10 +2,12 @@
 
 # call configure for debian8 specific settings
 ./configure --prefix=/usr \
---localstatedir=/var \
---libexecdir=${prefix}/lib/squid3 \
---datadir=${prefix}/share/squid3 \
---sysconfdir=/etc/squid3 \
---with-default-user=proxy \
---with-logdir=/var/log/squid3 \
---with-pidfile=/var/run/squid3.pid
+	--enable-http-violations \
+	--disable-header-reordering \
+	--localstatedir=/var \
+	--libexecdir=${prefix}/lib/squid3 \
+	--datadir=${prefix}/share/squid3 \
+	--sysconfdir=/etc/squid3 \
+	--with-default-user=proxy \
+	--with-logdir=/var/log/squid3 \
+	--with-pidfile=/var/run/squid3.pid
--- build-debian9-specific.sh
+++ build-debian9-specific.sh
@@ -3,7 +3,7 @@
 # call configure for debian9 specific settings
 ./configure --prefix=/usr \
 	--enable-http-violations \
-	--disable-header-reordering
+	--disable-header-reordering \
 	--localstatedir=/var \
 	--libexecdir=${prefix}/lib/squid \
 	--datadir=${prefix}/share/squid \
